0e28f88528c8e487d5e805f0401b89677519ec03
Michał Szablowski <michal.szablowski@nordicsemi.no>
2023-03-03 16:57:39 +0100

    Regenerate Certificates and enable configs for door lock
    * Generate PAA
    * Re-generate PAI
    * Re-generate DAC for 2 factory data
    * Generate and update Certification Declaration
    * Enable Factory Dataset by default
    * Enable Persistent Subscriptions by default

--- samples/matter/lock/gen_factory_data.sh
+++ samples/matter/lock/gen_factory_data.sh
@@ -4,10 +4,10 @@
 set -e
 cd $(dirname "$0")
 
-for suffix in "_1" "_2" "_3"; do
+for suffix in "_1" "_2"; do
   rm -rf build
   west build -b nrf52840dk_nrf52840 -t factory_data_hex -- -DOVERLAY_CONFIG="overlay-factory_data_build${suffix}.conf"
 
-  mkdir -p factory_data${suffix}
-  cp build/zephyr/{factory_data,CD,DAC,PAI}* factory_data${suffix}
+  mkdir -p factory_data_new${suffix}
+  cp build/zephyr/{factory_data}* factory_data_new${suffix}
 done
